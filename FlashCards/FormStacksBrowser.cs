9803376d12d4801f38a0638f5910ae7debdf85e4
Aleksander Korostelin <admin@flexits.info>
2021-10-03 18:42:51 +0500

    db test successful

--- FlashCards/FormStacksBrowser.cs
+++ FlashCards/FormStacksBrowser.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace FlashCards
 {
@@ -12,10 +13,34 @@ namespace FlashCards
     {
         public FormStacksBrowser()
         {
+            //https://metanit.com/sharp/adonetcore/4.1.php
+
             InitializeComponent();
+            string connstr = "Data Source=fcrd.db;Version=3;";
+            string query = "SELECT COUNT(*) FROM stacks";
+            SQLiteConnection conn = new SQLiteConnection(connstr);
+            SQLiteCommand cmd = new SQLiteCommand(query, conn);
+            conn.Open();
+            if (conn.State == ConnectionState.Open)
+            {
+
+            }
+            //long response = cmd.ExecuteScalar();
+            SQLiteDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            if (reader.HasRows)
+            {
+                reader.Read();
+            }
+            int result = reader.GetInt32(0);
+            reader.Close();
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
             //get stack_ids from database
             //populate panel with stackitems
-            int stackcounter = 8;
+            int stackcounter = result;
             int[] stack_ids = new int[stackcounter];
             stack_ids[0] = 0;
             stack_ids[1] = 1;
