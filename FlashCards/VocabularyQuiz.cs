1b6ffe8aeb1e0373a49adb4e7bf833675061a53d
Aleksander Korostelin <admin@flexits.info>
2021-10-19 17:01:43 +0500

    VocabQuiz class

--- FlashCards/VocabularyQuiz.cs
+++ FlashCards/VocabularyQuiz.cs
@@ -8,24 +8,58 @@ namespace FlashCards
     {
         public VocabQuiz(int[] card_ids)
         {
-            //make list of ids and populate it
-            //make list of returned cards (empty)
+            //make list of cardss (ids) and populate it
+            allcards = new List<int>(card_ids);
+            //make empty list of returned cards
+            returnedcards = new List<int>(card_ids.Length);
+            //init random
+            rnd = new Random();
         }
+
+        private List<int> allcards;
+        private List<int> returnedcards;
+        private Random rnd;
 
         public VocabCard PopRandomCard()
         {
-            //random selection of id from list
-            //if list is empty - populate it from returned cards
+            //random select card id from allcards
+            //return corresponding card and remove id from list
+            //if list is empty - populate it from returnedcards
             //if both are empty - return null
             //DbOperations.GetCardById(id)
-            //remove id from list
-            VocabCard vcard = null; ;
+            //
+            int index;
+            VocabCard vcard;
+
+            if (allcards.Count <= 0)
+            {
+                if (returnedcards.Count > 0)
+                {
+                    allcards.Clear();
+                    foreach (int id in returnedcards)
+                    {
+                        allcards.Add(id);
+                    }
+                    returnedcards.Clear();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            index = rnd.Next(0, allcards.Count - 1);
+            vcard = DbOperations.GetCardById(allcards[index]);
+            allcards.RemoveAt(index);
             return vcard;
         }
 
         public void PushCardBack(VocabCard vcard)
         {
             //add card id to the list of returned cards
+            if (!returnedcards.Contains(vcard.Id))
+            {
+                returnedcards.Add(vcard.Id);
+            }
         }
     }
 }
